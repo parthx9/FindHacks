457e55a859353ad6de63f4933cf40a45a6467216
Aditya Saxena <aditya.saxena33@gmail.com>
2021-06-01 22:35:16 +0530

    styling, filtering routes added

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,51 +5,50 @@ import "../index.css";
 const Card = ({ item }) => {
   return (
     <div>
-      <div className="card styledCard" style={{ backgroundColor: "" }}>
+      <div className="card styledCard">
         <div style={{ display: "flex", justifyContent: "space-between" }}>
           <img
-            class="card-img-top"
+            className="card-img-top imageIdentifier"
             src={item.image}
             alt="mlh"
-            style={{
-              width: "100%",
-              height: "10rem",
-            }}
           />
           <img
             src={item.companyLogo}
             alt="logo"
             style={{
               width: "10%",
-              height: "10%",
+              height: "8%",
               position: "absolute",
-              right: "0px",
+              right: "51px",
+              top: "49px",
+              borderRadius: "7px",
             }}
           />
         </div>
 
         <div class="card-body">
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <h4 class="card-title">{item.title}</h4>
-            <h5>{item.date}</h5>
-          </div>
+          <div className="card innerCard">
+            <div className="card-body">
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <h4 class="card-title">{item.title}</h4>
+                <h5>{item.date}</h5>
+              </div>
 
-          <p class="card-text">
-            {item.participants !== "" ? item.participants : null}
-            {item.prize !== "" ? " Prize: " + item.prize : null}
-          </p>
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <a
-              href={item.link}
-              target="_blank"
-              rel="noreferrer"
-              class="btn btn-primary"
-            >
-              Visit Website
-            </a>
-            <p style={{ position: "relative", top: "0.5rem" }}>
-              Powered By: {item.company}
-            </p>
+              <p class="card-text">
+                {item.participants !== "" ? item.participants : null}
+                {item.prize !== "" ? " Prize: " + item.prize : null}
+              </p>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <a
+                  href={item.link}
+                  target="_blank"
+                  rel="noreferrer"
+                  class="btn specialButton"
+                >
+                  Visit Website
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/CardsComponent.js
+++ src/components/CardsComponent.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Card from "./Card";
 import axios from "axios";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const CardsComponent = () => {
   const [data, setData] = useState([]);
+  const [company, setCompany] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,15 +18,43 @@ const CardsComponent = () => {
     };
     fetchData();
   }, []);
-  console.log(data);
+
+  const handleChange = (e) => {
+    const customData = async () => {
+      await axios
+        .get(`https://allhacks.herokuapp.com/${e.target.value}`)
+        .then((res) => setData(res.data.hackathon));
+    };
+    customData();
+  };
+
   return (
-    <div className="grid-container">
-      {data?.map((item) => (
-        <div>
-          <Card item={item} />
-        </div>
-      ))}
-    </div>
+    <>
+      <FormControl
+        className="test"
+        style={{ marginLeft: "45rem", width: "7rem", color: "white" }}
+      >
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={company}
+          onChange={handleChange}
+        >
+          <MenuItem value={"devfolio"}>Devfolio</MenuItem>
+          <MenuItem value={"mlh"}>MLH</MenuItem>
+          <MenuItem value={"devpost"}>Devpost</MenuItem>
+          <MenuItem value={"eventbrite"}>Event Brite</MenuItem>
+        </Select>
+      </FormControl>
+      <div className="grid-container">
+        {data?.map((item) => (
+          <div>
+            <Card item={item} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
