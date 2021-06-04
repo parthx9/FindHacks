bb7f5ad8ac33bd3cd84978166dbf79cc8fcd2d35
Lucas Carrias <lucascarrias@outlook.com>
2021-06-04 00:04:18 -0300

    Create Header component test

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,21 @@
 const Header = () => {
   return (
-    <div className="header">
+    <div className="header" data-testid="header">
       <div>
-        <div className="brand">
-          Find Hacks
-        </div>
+        <div className="brand">Find Hacks</div>
         <div className="subtitle">
           Find all hackathons at once! Happy Hacking.
         </div>
       </div>
 
-      <div className='github'>
-        <a target='_blank' rel="noreferrer" href='https://github.com/parthx9/allHacks'><i className='fa-3x fa fa-github'></i></a>
+      <div className="github">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/parthx9/allHacks"
+        >
+          <i className="fa-3x fa fa-github"></i>
+        </a>
       </div>
     </div>
   );
